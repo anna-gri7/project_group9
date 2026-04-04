03c8aebef7e709f3c758c439df05ee84ac59a84a
Oleksandr <kksasha201507@gmail.com>
2026-04-04 11:26:21 +0200

    header-test

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,1 +1,20 @@
-// відкриття і закриття бургер-меню + блокування скролу сторінки 
+const openBtn = document.querySelector('[data-menu-open]');
+const closeBtn = document.querySelector('[data-menu-close]');
+const menu = document.querySelector('[data-menu]');
+
+function openMenu() {
+  menu.classList.add('is-open');
+  document.body.classList.add('no-scroll');
+}
+
+function closeMenu() {
+  menu.classList.remove('is-open');
+  document.body.classList.remove('no-scroll');
+}
+
+openBtn.addEventListener('click', openMenu);
+closeBtn.addEventListener('click', closeMenu);
+
+menu.addEventListener('click', (e) => {
+  if (e.target === menu) closeMenu();
+});
